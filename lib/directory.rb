cc1b19ce8389fbe6941784fbbcfdeb1740efd960
yuanying <yuanying@fraction.jp>
2012-02-07 14:13:10 +0900

    パフォーマンスを改善。

--- lib/directory.rb
+++ lib/directory.rb
@@ -2,28 +2,37 @@
 class Directory < Filesystem
 
   attr_accessor :path
-  attr_accessor :children
-  attr_accessor :images
 
   def initialize path, parent=nil
     super
-    @children = []
-    @images   = []
+  end
+
+  def children
     create_index
+    @children
+  end
+
+  def images
+    create_index
+    @images
   end
 
   def create_index
-    Dir.foreach(real_path) do |file|
-      next if file.start_with?('.')
-      real_file_path = File.join(real_path, file)
-      if File.directory?(real_file_path)
-        @children << Directory.new(File.join(path, file))
-      elsif File.file?(real_file_path) && Image::SUPPORTED_FORMATS.include?(File.extname(real_file_path))
-        @images << Image.new(File.join(path, file), self)
+    unless defined?(@children) && defined?(@images)
+      @children = []
+      @images   = []
+      Dir.foreach(real_path) do |file|
+        next if file.start_with?('.')
+        real_file_path = File.join(real_path, file)
+        if File.directory?(real_file_path)
+          @children << Directory.new(File.join(path, file))
+        elsif File.file?(real_file_path) && Image::SUPPORTED_FORMATS.include?(File.extname(real_file_path))
+          @images << Image.new(File.join(path, file), self)
+        end
       end
+      @children.sort!
+      @images.sort!
     end
-    @children.sort!
-    @images.sort!
   end
 
 end
